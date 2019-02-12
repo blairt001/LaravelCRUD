a86e456749edaa4b4e8b39a250c3c627472a8d08
Tony Okoth <blairt371.dev@gmail.com>
2019-02-12 23:14:30 +0300

    Sets up models and routes for blog posts

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'BlogController@home');
+Route::get('/home', 'BlogController@index')->name('home');
+Route::resources('/posts','BlogController');
