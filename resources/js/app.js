06e38d55f6103dbe10be201332fb07c9cd4e3480
Tony Okoth <blairt371.dev@gmail.com>
2019-02-13 00:35:40 +0300

    Created Vue app

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,9 +11,22 @@ window.VueAxios = require('vue-axios').default;
 Window.Axios = require('axios').default;
 
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-
+/*
 const app = new Vue({
     el: '#app'
 });
+*/
+
+Vue.use(VueRouter,VueAxios,axios);
+
+
+const router = new VueRouter({mode: 'history', routes: routes});
+
+new Vue(
+    Vue.util.extend({
+        router },
+        AppLayout)
+).$mount(#app)
+
